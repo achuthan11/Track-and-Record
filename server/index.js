c29d421a83efab9d00ec2b33fb1a5812066aa7ac
achuthan11 <2000achuthans@gmail.com>
2021-12-25 09:19:29 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json()); // to parse the incoming data from json
+app.use(cors());  // to use cross platform data, i.e from front end to backend
+
+const db= mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "Kratoswithsword2",
+    database: "track_and_record"
+});
+
+app.post('/login', (req,res) => {
+    const email = req.body.email;
+    const password = req.body.password
+    db.query(
+        "select email, password from employee where email=? and password =?",
+        [email,password],
+        (err,result)=>{
+            if(err){
+                console.log({err :err});
+            }
+            if(result) {
+                res.send(result);
+            }
+            else{
+                res.send({message: "Invalid credentials"});
+            }
+        }
+    )
+});
+
+app.get('/',(req,res)=>{
+    const query="select * from employee";
+    db.query(query,(err,result) => {
+        if(err)  return res.status(404).send(err);
+        else res.send(result);
+    });
+});
+
+app.listen(3001, ()=>{
+    console.log("Running on 3001");
+});
