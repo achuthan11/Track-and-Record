3d2912fba1710916eda413c4186ddc1369a64266
achuthan11 <2000achuthans@gmail.com>
2021-12-26 10:42:33 +0530

    add formatting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,26 @@
-import './App.css';
-import {BrowserRouter as Router, Route,Switch} from "react-router-dom";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./LoginForm";
 import Home from "./HomePage";
 
 function App() {
-  return <>
-    <Router>
-      <Switch>
-      <Route exact path="/">
-        <Login/>
-      </Route>
-      <Route  path="/home">
-        <Home/>
-      </Route>
-      <Route path="*">
-        <h2>Error page</h2>
-      </Route>
-      </Switch>
-    </Router>
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="*">
+            <h2>Error page</h2>
+          </Route>
+        </Switch>
+      </Router>
     </>
+  );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,48 +1,47 @@
-const express = require('express');
-const mysql = require('mysql');
-const cors = require('cors');
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json()); // to parse the incoming data from json
-app.use(cors());  // to use cross platform data, i.e from front end to backend
+app.use(cors()); // to use cross platform data, i.e from front end to backend
 
-const db= mysql.createConnection({
-    user: "root",
-    host: "localhost",
-    password: "Kratoswithsword2",
-    database: "track_and_record"
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "Kratoswithsword2",
+  database: "track_and_record",
 });
 
-app.post('/login', (req,res) => {
-    const email = req.body.email;
-    const password = req.body.password
-    db.query(
-        "select email, password from employee where email=? and password =?",
-        [email,password],
-        (err,result)=>{
-            if(err){
-                console.log({err :err});
-            }
-            if(result) {
-                res.send(result);
-                console.log(result[1]);
-            }
-            else{
-                res.send({message: "Invalid credentials"});
-                console.log("Error message", result);
-            }
-        }
-    )
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  db.query(
+    "select email, password from employee where email=? and password =?",
+    [email, password],
+    (err, result) => {
+      if (err) {
+        console.log({ err: err });
+      }
+      if (result) {
+        res.send(result);
+        console.log(result[1]);
+      } else {
+        res.send({ message: "Invalid credentials" });
+        console.log("Error message", result);
+      }
+    }
+  );
 });
 
-app.get('/',(req,res)=>{
-    const query="select * from employee";
-    db.query(query,(err,result) => {
-        if(err)  return res.status(404).send(err);
-        else res.send(result);
-    });
+app.get("/", (req, res) => {
+  const query = "select * from employee";
+  db.query(query, (err, result) => {
+    if (err) return res.status(404).send(err);
+    else res.send(result);
+  });
 });
 
-app.listen(3001, ()=>{
-    console.log("Running on 3001");
+app.listen(3001, () => {
+  console.log("Running on 3001");
 });
